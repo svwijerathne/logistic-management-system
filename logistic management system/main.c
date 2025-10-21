0f360d50c63b57e6620a9689e180c744dfc2e0c6
svWijerathne <as20240665@sci.sjp.ac.lk>
2025-10-21 12:22:40 +0530

    added distance management

--- logistic management system/main.c
+++ logistic management system/main.c
@@ -16,9 +16,14 @@ void cityManagement(char cities[MAX_CITIES][100],int *cityCount,int postalCode[M
 void addCity(char cities[MAX_CITIES][100],int *cityCount,int postalCode[MAX_CITIES]);
 void renameCity(char cities[MAX_CITIES][100],int *cityCount,int postalCode[MAX_CITIES]);
 void removeCity(char cities[MAX_CITIES][100],int *cityCount,int postalCode[MAX_CITIES]);
+void distanceManagement(int distance[MAX_CITIES][MAX_CITIES],char cities[MAX_CITIES][100],int cityCount,int postalCodes[MAX_CITIES]);
+void displayDistanceTable(int cityCount,char cities[MAX_CITIES][100],int distance[MAX_CITIES][MAX_CITIES]);
+void inputDistance(int cityCount,char cities[MAX_CITIES][100],int distance[MAX_CITIES][MAX_CITIES],int postalCodes[MAX_CITIES]);
+int findCityIndexByPostal(int code, int cityCount, int postalCodes[MAX_CITIES]);
 
 int main(void) {
     char cities[MAX_CITIES][100];
+    int distance[MAX_CITIES][MAX_CITIES];
     int cityCount=0;
     int postalCode[MAX_CITIES];
     int choice=0;
@@ -44,6 +49,7 @@ int main(void) {
                 break;
             case 2:
                 //distance management
+                distanceManagement(distance,cities,cityCount,postalCode);
                 break;
             case 3:
                 //vehicle management
@@ -91,9 +97,15 @@ void cityManagement(char cities[MAX_CITIES][100],int *cityCount,int postalCode[M
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1: addCity(cities, cityCount, postalCode); break;
-            case 2: renameCity(cities, cityCount, postalCode); break;
-            case 3: removeCity(cities, cityCount, postalCode); break;
+            case 1:
+                addCity(cities, cityCount, postalCode);
+                break;
+            case 2:
+                renameCity(cities, cityCount, postalCode);
+                break;
+            case 3:
+                removeCity(cities, cityCount, postalCode);
+                break;
             case 4:
                 printf("\nCities List:\n");
                 printf("Postal code \t\t city \n");
@@ -186,3 +198,102 @@ void removeCity(char cities[MAX_CITIES][100], int *cityCount, int postalCode[MAX
     }
 }
 
+
+int findCityIndexByPostal(int code, int cityCount, int postalCodes[MAX_CITIES]) {
+    for (int i = 0; i < cityCount; i++) {
+        if (postalCodes[i] == code)
+            return i;
+    }
+    return -1;  // Not found
+}
+
+void distanceManagement(int distance[MAX_CITIES][MAX_CITIES],
+                        char cities[MAX_CITIES][100],
+                        int cityCount,
+                        int postalCodes[MAX_CITIES]) {
+    int choice;
+    do {
+        printf("\n--- Distance Management ---\n");
+        printf("1. Enter Distance Between Cities\n");
+        printf("2. View Distance Table\n");
+        printf("3. Back\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                if (cityCount < 2) {
+                    printf("At least 2 cities required to add distances!\n");
+                } else {
+                    inputDistance(cityCount, cities, distance, postalCodes);
+                }
+                break;
+            case 2:
+                displayDistanceTable(cityCount, cities, distance);
+                break;
+            case 3:
+                printf("Returning to main menu...\n");
+                break;
+            default:
+                printf("Invalid choice! Try again.\n");
+        }
+    } while (choice != 3);
+}
+
+void inputDistance(int cityCount, char cities[MAX_CITIES][100], int distance[MAX_CITIES][MAX_CITIES], int postalCodes[MAX_CITIES]) {
+    int srcCode, destCode;
+    printf("Enter source city postal code: ");
+    scanf("%d", &srcCode);
+    printf("Enter destination city postal code: ");
+    scanf("%d", &destCode);
+
+    // find the corresponding indices
+    int srcIndex = findCityIndexByPostal(srcCode, cityCount, postalCodes);
+    int destIndex = findCityIndexByPostal(destCode, cityCount, postalCodes);
+
+    
+    if (srcIndex == -1) {
+        printf("source city postal codes not found!\n");
+        return;
+    }
+    
+    if ( destIndex == -1) {
+        printf("destination city postal codes not found!\n");
+        return;
+    }
+    
+    if (srcIndex == destIndex) {
+        printf("Source and destination cannot be the same city!\n");
+        return;
+    }
+
+    int dist;
+    printf("Enter distance (km): ");
+    scanf("%d", &dist);
+
+    distance[srcIndex][destIndex] = dist;
+    distance[destIndex][srcIndex] = dist;
+    printf("Distance successfully updated between %s and %s.\n", cities[srcIndex], cities[destIndex]);
+}
+
+void displayDistanceTable(int cityCount, char cities[MAX_CITIES][100], int distance[MAX_CITIES][MAX_CITIES]) {
+    if (cityCount == 0) {
+        printf("No cities available.\n");
+        return;
+    }
+
+    printf("\n=========== Distance Table (km) ==================\n");
+    printf("%-15s", "City");
+    for (int i = 0; i < cityCount; i++)
+        printf("%18s", cities[i]);
+    printf("\n");
+
+    for (int i = 0; i < cityCount; i++) {
+        printf("%-18s", cities[i]);
+        for (int j = 0; j < cityCount; j++) {
+            printf("%18d", distance[i][j]);
+        }
+        printf("\n");
+    }
+    printf("====================================================\n");
+}
